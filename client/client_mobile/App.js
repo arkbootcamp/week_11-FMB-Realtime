53eeeb8352a2f6b142fa9c20f2d5dfd229d28fc4
Fakhri <fakhridho95@gmail.com>
2020-10-05 17:39:59 +0700

    notif

--- client/client_mobile/App.js
+++ client/client_mobile/App.js
@@ -1,114 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+  showLocalNotification,
+  showLocalNotificationScheduled,
+  cancelAllNotification,
+} from './src/handleNotification';
+import PushNotification from 'react-native-push-notification';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const channelId = 'test-notification';
 
-const App: () => React$Node = () => {
+  useEffect(() => {
+    PushNotification.createChannel(
+      {
+        channelId,
+        channelName: 'test notification',
+      },
+      (created) => console.log(`createChannel returned '${created}'`),
+      // (optional) callback returns whether the channel was created, false means it already existed.
+    );
+  }, []);
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.container}>
+      <Text>Push Notification</Text>
+      <TouchableOpacity
+        activeOpacity={0.6}
+        onPress={() =>
+          showLocalNotification('New Notification', 'Selamat Datang', channelId)
+        }>
+        <View style={styles.button}>
+          <Text style={styles.buttonTitle}>
+            Click to Get Notification right now
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.6}
+        onPress={() =>
+          showLocalNotificationScheduled(
+            'Notification From the Past',
+            'Hello From 5 Seconds Ago',
+            channelId,
+          )
+        }>
+        <View style={styles.button}>
+          <Text style={styles.buttonTitle}>
+            Click to Get Notification 5 seconds later
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.6} onPress={cancelAllNotification}>
+        <View style={styles.button}>
+          <Text style={styles.buttonTitle}>
+            Click to Cancel All Notification
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  button: {
+    padding: 16,
+    backgroundColor: 'blue',
+    borderRadius: 24,
+    marginTop: 10,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  buttonTitle: {
+    color: 'white',
   },
 });
-
-export default App;
